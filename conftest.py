635d39649b2eb43b2f187a9fa23e60ef91cdba9e
Anton Z <johndoe@example.com>
2021-10-17 16:12:07 +0300

    Edit test main_page for Page Object

---
+++ conftest.py
@@ -0,0 +1,26 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--language',
+        action='store',
+        default='en',
+        help='Choose languages'
+    )
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    options = Options()
+    user_language = request.config.getoption('language')
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    browser = webdriver.Chrome(
+        executable_path='/Users/antonzimaev/Downloads/chromedriver-1',
+        options=options)
+    browser.implicitly_wait(10)
+    yield browser
+    browser.close()
+    browser.quit()
